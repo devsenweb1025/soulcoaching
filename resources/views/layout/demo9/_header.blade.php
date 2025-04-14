96a88fd4c9633c493e1fc9264bd4abb103aea01e
sduflo <sduflo@mht-technologies.com>
2025-04-14 15:06:54 -0500

    update cart in mobile view

--- resources/views/layout/demo9/_header.blade.php
+++ resources/views/layout/demo9/_header.blade.php
@@ -27,6 +27,17 @@
                     {{ theme()->getView('partials/topbar/_notifications-menu') }}
                 </div>
 
+                <!--begin::Chat App item-->
+                <div class="me-3">
+                    <a href="#" class="btn btn-icon btn-custom btn-active-color-primary position-relative"
+                        id="kt_drawer_chat_toggle">
+                        {!! theme()->getIcon('message-text-2', 'fs-1 text-white') !!}
+                        <span
+                            class="bullet bullet-dot bg-success h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
+                    </a>
+                </div>
+                <!--end::Chat App item-->
+
                 <div class="me-3">
                     <a href="#" class="btn btn-icon btn-custom btn-active-color-primary"
                         data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
@@ -34,27 +45,6 @@
                     </a>
                     {{ theme()->getView('partials/topbar/_user-menu', ['language-menu-placement' => 'right-start', 'subscription-menu-placement' => 'right-start']) }}
                 </div>
-
-                <!--begin::Theme Mode item-->
-                <div class="me-3">
-                    <a href="#" class="btn btn-icon btn-custom btn-active-color-primary"
-                        data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-                        {!! theme()->getIcon('night-day', 'fs-1 text-white theme-light-show') !!}
-                        {!! theme()->getIcon('moon', 'fs-1 text-white theme-dark-show') !!}
-                    </a>
-                    @include('partials/theme-mode/__menu')
-                </div>
-                <!--end::Theme Mode item-->
-
-                <!--begin::Chat App item-->
-                <div class="me-3">
-                    <a href="#" class="btn btn-icon btn-custom btn-active-color-primary position-relative" id="kt_drawer_chat_toggle">
-                        {!! theme()->getIcon('message-text-2', 'fs-1 text-white') !!}
-                        <span
-                            class="bullet bullet-dot bg-success h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
-                    </a>
-                </div>
-                <!--end::Chat App item-->
             </div>
             <!--end::Action-->
         </div>
--- resources/views/layout/landing/master.blade.php
+++ resources/views/layout/landing/master.blade.php
@@ -17,6 +17,7 @@
             {{ $slot }}
             @include('layout.landing._footer')
             @include('layout.landing._audio')
+            @include('layout.landing._cart')
         </div>
         <!--end::Root-->
 
