d73a4d19ea7ec5b2dafd2bb828385464120e314c
sduflo <sduflo@mht-technologies.com>
2025-04-15 13:26:11 -0500

    footer update

--- resources/views/layout/demo9/_footer.blade.php
+++ resources/views/layout/demo9/_footer.blade.php
@@ -3,21 +3,11 @@
     <!--begin::Container-->
     <div class="{{ theme()->printHtmlClasses('footer-container', false) }} d-flex flex-column flex-md-row flex-stack">
         <!--begin::Copyright-->
-        <div class="text-dark order-2 order-md-1">
-            <span class="text-gray-400 fw-bold me-1">Created by</span>
-            <a href="{{ theme()->getOption('general', 'website') }}" target="_blank" class="text-muted text-hover-primary fw-bold me-2 fs-6">Keenthemes</a>
+        <div class="text-gray-600 fs-5">
+            <span class="text-gray-600">Website wurde erstellt von </span>
+            <a href="https://magentix.ch" target="_blank" class="text-gray-600 text-hover-primary">MAGENTIX</a>
         </div>
         <!--end::Copyright-->
-
-        <!--begin::Menu-->
-        <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
-            <li class="menu-item"><a href=" {{ theme()->getOption('general', 'about') }}" target="_blank" class="menu-link px-2">About</a></li>
-
-            <li class="menu-item"><a href=" {{ theme()->getOption('general', 'support') }}" target="_blank" class="menu-link px-2">Support</a></li>
-
-            <li class="menu-item"><a href=" {{ theme()->getOption('product', 'purchase') }}" target="_blank" class="menu-link px-2">Purchase</a></li>
-        </ul>
-        <!--end::Menu-->
     </div>
     <!--end::Container-->
 </div>
