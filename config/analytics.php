92a8e159034b7d74c16a6f17637092a68b6c9b9c
sduflo <sduflo@mht-technologies.com>
2025-04-16 13:19:50 -0500

    google analytics added

---
+++ config/analytics.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Google Analytics
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify your Google Analytics tracking ID and other settings.
+    |
+    */
+
+    'tracking_id' => env('GOOGLE_ANALYTICS_ID', null),
+    'enabled' => env('GOOGLE_ANALYTICS_ENABLED', true),
+];
