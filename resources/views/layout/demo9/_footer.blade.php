d6ff1e306b2bcc1f62dd721361199ada01d241a9
m-joel <m.joel.walek@gmailcom>
2025-04-21 15:14:51 -0500

    footer and meta data updated

--- resources/views/layout/demo9/_footer.blade.php
+++ resources/views/layout/demo9/_footer.blade.php
@@ -3,10 +3,6 @@
     <!--begin::Container-->
     <div class="{{ theme()->printHtmlClasses('footer-container', false) }} d-flex flex-column flex-md-row flex-stack justify-content-center">
         <!--begin::Copyright-->
-        <div class="text-gray-600 fs-5">
-            <span class="text-gray-600">Website wurde erstellt von </span>
-            <a href="https://magentix.ch" target="_blank" class="text-gray-800 text-hover-primary">MAGENTIX</a>
-        </div>
         <!--end::Copyright-->
     </div>
     <!--end::Container-->
