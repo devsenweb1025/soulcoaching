21f65f870dcdef69376ac7e5ea0b4f6a68f054da
m-joel <m.joel.walek@gmail.com>
2025-05-27 09:38:45 -0500

    remove newsletter functions

--- resources/views/pages/landing/index.blade.php
+++ resources/views/pages/landing/index.blade.php
@@ -9,6 +9,4 @@
     @include('pages.landing._partials._whyme')
     @include('pages.landing._partials._reviews')
     @include('pages.landing._partials._faqs')
-    <script
-        id="mcjs">!function (c, h, i, m, p) { m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p) }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/023af728638dde10ccacc1461/819f4767b84230c4d6ee91ca6.js");</script>
 </x-landing-layout>
